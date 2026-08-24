187f36891de6942bae95e03f7415d4d4601723f8
Muhammad Faris Khan <khangee90870@gmail.com>
2026-08-24 14:02:40 +0000

    data_types_updation

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert(3 + 3) we are using node js not browser.
+
+console.log( 3
+     +
+      3); // code readability should be high 
+
+console.log("Faris khan");
+
+/* 
+Docomentation
+->mdn
+->tc39
+*/
+
+let name = "Faris";
+let age = 18;
+let isloggedIn = true;
+let state = null;
+let city;
+
+/*
+number => 2 to power 53
+bigimt
+string => ""
+boolean => true/false
+null => stand Alone value
+undefined => 
+symbol => unique value
+*/
+
+/*
+
+object
+
+*/
+console.log(typeof null);
