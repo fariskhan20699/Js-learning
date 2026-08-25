6b1e6fdafcca492fd2b3eecc7219547ee6f71751
Muhammad Faris Khan <khangee90870@gmail.com>
2026-08-25 14:00:41 +0000

    operation file update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "true"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber); //NaN (Not A Number)
+// console.log(valueInNumber); //NaN (Not A Number)
 
 
 /* 
@@ -17,8 +17,8 @@ true => 1; false => 0
 let isLoggedIn = "jkl";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /* 
 1 => true
@@ -29,11 +29,63 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = null;
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 /*
 33 => "33"
 true => "true"
 null => "null"
 */
+
+//===================== Operations ===============================
+
+let value = 5;
+let negValue = -value;
+// console.log(negValue);
+
+/* 
+Basic operations
+    console.log(2 + 2);
+    console.log(2 - 2);
+    console.log(2 * 2);
+    console.log(2 ** 2);
+    console.log(2 / 2);
+    console.log(2 % 2);
+*/
+let str1 = "Hello ";
+let str2 = "Faris";
+let str3 = str1 + str2;
+//console.log(str3);
+
+//to primitive case
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log(1 + 2 + "2"); //32
+// console.log(1 + "2" + 2); //122
+// console.log("1" + 2 + 2); //122
+
+// console.log(+true);
+// console.log(+"");
+
+//foucus on code readablity don't do it complex thing
+let num1 , num2 ,num3;
+ num1 = num2 = num3 = 2 + 3; //confusion bad coding 
+
+ let counter = 100;
+ ++counter;
+ console.log(counter);
+ 
+ //Link to study: 
+ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+ 
+
+
+
+
+
+
+
+
+
