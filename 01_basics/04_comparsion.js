6438c92fd97b4eefea044218509cef867da19d25
Muhammad Faris Khan <khangee90870@gmail.com>
2026-08-25 14:21:52 +0000

    comaprsion file added

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,22 @@
+//basic comparsion
+//console.table([2 > 1,2 < 1,2 <= 1,2 >= 1,2 == 1,2 != 1]);
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+//cunfusion avoid 
+/*console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+*/
+//===
+console.log("03" > 4); //false
+
+
+
+
+
